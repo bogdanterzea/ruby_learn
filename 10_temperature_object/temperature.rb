8577e061e3a81c8c94c0773d1fd4f168ce215ed3
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2020-04-01 18:21:04 +0300

    Update 10_temperature

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -4,37 +4,43 @@ class Temperature
   def initialize(hash)
     @ftemp = hash[:f]
     @ctemp = hash[:c]
-    @ftemp = (@ctemp.to_f * 9 / 5) + 32 unless @ctemp.nil?
-    @ctemp = (@ftemp - 32) * 5 / 9 unless @ftemp.nil?
   end
 
   def in_fahrenheit
-    @ftemp
+    if @ftemp != 0
+      @ftemp
+    else
+      (@ctemp.to_f * 9 / 5) + 32
+    end
   end
 
   def in_celsius
-    @ctemp
+    if @ftemp != 0
+      (@ftemp - 32) * 5 / 9
+    else
+      @ctemp
+    end
   end
 
   def self.from_celsius(temp)
-    self.new(:c => temp)
+    new(:c => temp)
   end
 
   def self.from_fahrenheit(temp)
-    self.new(:f => temp)
+    new(:f => temp)
   end
 end
 
 class Celsius < Temperature
   def initialize(temp)
     @ctemp = temp
-    @ftemp = (@ctemp.to_f * 9 / 5) + 32 unless @ctemp.nil?
+    @ftemp = in_fahrenheit
   end
 end
 
 class Fahrenheit < Temperature
   def initialize(temp)
     @ftemp = temp
-    @ctemp = (@ftemp - 32) * 5 / 9 unless @ftemp.nil?
+    @ctemp = in_celsius
   end
 end
